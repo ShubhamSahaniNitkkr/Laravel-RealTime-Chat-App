043ad3ca6e70334191af2f5260b2d22565d25dd2
Shubham Sunny <shubhamsahaninitkkr@gmail.com>
2019-03-28 11:22:21 +0530

    everything working till now

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/chat', 'ChatController@index')->middleware('auth')->name('chat.index');
+// Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'ChatController@index')->middleware('auth')->name('chat.index');
 Route::get('/chat/{id}', 'ChatController@show')->middleware('auth')->name('chat.show');
 Route::post('/chat/getChat/{id}', 'ChatController@getChat')->middleware('auth');
 Route::post('/chat/sendChat', 'ChatController@sendChat')->middleware('auth');
